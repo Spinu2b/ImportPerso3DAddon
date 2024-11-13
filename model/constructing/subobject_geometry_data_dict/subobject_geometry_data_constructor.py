d5119cfd1bc19fab2a8fe410561658a9531f7895
Spinu2b <spinu2b@gmail.com>
2024-11-13 20:44:49 +0100

    constructing data in-memory in-dev

---
+++ model/constructing/subobject_geometry_data_dict/subobject_geometry_data_constructor.py
@@ -0,0 +1,9 @@
+
+
+class SubobjectGeometryDataConstructor:
+    def __init__(self):
+        self.result = None  # type: SubobjectGeometryData
+
+    def construct_from_json(self, subobject_geometry_data_dict):
+        raise NotImplementedError
+        
--- model/constructing/subobject_geometry_data_dict/subobject_geometry_data_dict_constructor.py
+++ model/constructing/subobject_geometry_data_dict/subobject_geometry_data_dict_constructor.py
@@ -1,3 +1,4 @@
+from model.constructing.subobject_geometry_data_dict.subobject_geometry_data_constructor import SubobjectGeometryDataConstructor
 
 
 class SubobjectGeometryDataDictConstructor:
@@ -5,4 +6,7 @@ class SubobjectGeometryDataDictConstructor:
         self.result = dict()  # type: Dict[str, SubobjectGeometryData]
 
     def construct_from_json(self, subobject_geometry_data_dict):
-        raise NotImplementedError
+        for subobject_geometry_data_hash in subobject_geometry_data_dict:
+            self.result[subobject_geometry_data_hash] = \
+                SubobjectGeometryDataConstructor().construct_from_json(subobject_geometry_data_dict[subobject_geometry_data_hash])
+        return self.result
