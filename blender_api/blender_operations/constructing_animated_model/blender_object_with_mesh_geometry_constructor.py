e1af00848bc3b55e4efb6fc056b4f14d850bc8a7
Spinu2b <spinu2b@gmail.com>
2024-11-17 00:04:27 +0100

    save, applying materials

--- blender_api/blender_operations/constructing_animated_model/blender_object_with_mesh_geometry_constructor.py
+++ blender_api/blender_operations/constructing_animated_model/blender_object_with_mesh_geometry_constructor.py
@@ -1,9 +1,14 @@
+from typing import List
 from blender_api.blender_operations.general_api_operations.blender_objects_manipulation import BlenderObjectsManipulation
 import bpy
 from bpy.types import Object, MeshUVLoopLayer, Image, Node
 
 from model.perso.subobject_geometry_data import SubobjectGeometryData
 from model.perso.subobjects_compressed_perso_3d_animated_data import SubobjectsCompressedPerso3DAnimatedData
+
+
+class BlenderMeshMaterialApplier:
+    
 
 
 class BlenderObjectWithMeshGeometryConstructor:
@@ -24,4 +29,22 @@ class BlenderObjectWithMeshGeometryConstructor:
 
         mesh_obj.data.from_pydata(vertices, edges, faces)
         mesh_obj.animation_data_create()
+
+        self._apply_normals(subobject_geometry_data, mesh_obj)
+        self._apply_mesh_materials(subobject_geometry_data, mesh_obj)
         return mesh_obj
+    
+    def _apply_mesh_materials(self, subobject_geometry_data: SubobjectGeometryData, mesh_obj: Object):
+        blender_mesh_material_applier = BlenderMeshMaterialApplier()
+        blender_mesh_material_applier.apply(
+            uv_map=subobject_geometry_data.uv_map,
+            mesh_obj = mesh_obj,
+            subobject_geometry_data=subobject_geometry_data
+        )
+
+    def _apply_normals(self, subobject_geometry_data: SubobjectGeometryData, mesh_obj: Object):
+        normals_definitions = subobject_geometry_data.normals  # type: List[Vector3d]
+        for mesh_vertex_index, mesh_vertex in enumerate(mesh_obj.data.vertices):
+            mesh_vertex.normal[0] = normals_definitions[mesh_vertex_index].x
+            mesh_vertex.normal[1] = normals_definitions[mesh_vertex_index].y
+            mesh_vertex.normal[2] = normals_definitions[mesh_vertex_index].z
